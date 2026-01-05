a963279c157e67a474f2d19f3180faffa19df308
NAD3R <naderdeghaili@gmail.com>
2026-01-05 16:58:29 +0100

    doing 3

--- src/Esercizio3.java
+++ src/Esercizio3.java
@@ -1,3 +1,20 @@
 public class Esercizio3 {
+    public static double perimetroRettangolo(double base, double altezza) {
+        return (base + altezza) * 2;
+    }
+
+    public static int pariDispari(int n) {
+        if (n % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // formula di Erone: Area = \(\sqrt{p(p-a)(p-b)(p-c)}\)
+    public static double perimetroTriangolo(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
     
 }
